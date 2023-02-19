e1f00e2b5b57b8a225a57748a21c9291f415ecce
dineshpriyantha <dinesh.priyantha.uom@gmail.com>
2023-02-19 14:05:22 +0900

    Adding an activity interface

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 import { Header } from 'semantic-ui-react';
 import List from 'semantic-ui-react/dist/commonjs/elements/List';
 import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
+import { Activity } from '../models/activity';
 
 function App() {
-const [activities, setActivities] = useState([]);
+const [activities, setActivities] = useState<Activity[]>([]);
 
 useEffect(() => {
-  axios.get('http://localhost:5000/api/devhubblogs')
-   .then(response => {
+  axios.get<Activity[]>('http://localhost:5000/api/devhubblogs').then(response => {
     setActivities(response.data);  // set data to 'activites' variable
    })
 }, [])
@@ -18,7 +18,7 @@ useEffect(() => {
     <div>
       <Header as='h2' icon='users' content='Dev Hub' />           
         <List>
-          {activities.map((activity: any) => (
+          {activities.map(activity => (
             <List.Item key={activity.id}>
               {activity.title}
             </List.Item>
